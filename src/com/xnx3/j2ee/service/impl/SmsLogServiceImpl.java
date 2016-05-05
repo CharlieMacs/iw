0a9f6e61bae135144976c0aa6a2a8f152a1716e8
xnx3 <mail@xnx3.com>
2016-05-05 21:06:58 +0800

    xnx3

--- src/com/xnx3/j2ee/service/impl/SmsLogServiceImpl.java
+++ src/com/xnx3/j2ee/service/impl/SmsLogServiceImpl.java
@@ -1,14 +1,20 @@
 package com.xnx3.j2ee.service.impl;
 
 import java.util.List;
+import java.util.Random;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Service;
 
+import com.xnx3.DateUtil;
+import com.xnx3.SendPhoneMsgUtil;
 import com.xnx3.j2ee.dao.SmsLogDAO;
 import com.xnx3.j2ee.entity.SmsLog;
 import com.xnx3.j2ee.service.SmsLogService;
+import com.xnx3.j2ee.util.IpUtil;
+import com.xnx3.j2ee.vo.BaseVO;
 
 @Service("smsLogService")
 public class SmsLogServiceImpl implements SmsLogService {
@@ -123,6 +129,74 @@ public class SmsLogServiceImpl implements SmsLogService {
 	 */
 	public List findByPhoneAddtimeUsedType(String phone,int addtime,Short used,Short type,String code){
 		return smsLogDAO.findByPhoneAddtimeUsedType(phone, addtime, used, type,code);
+	}
+
+	/**
+	 * 发送手机号登录的验证码
+	 * @param request {@link HttpServletRequest}
+	 * 			<br/>form表单需提交参数：phone(发送到的手机号)
+	 * @return {@link BaseVO}
+	 */
+	@Override
+	public BaseVO sendPhoneLoginCode(HttpServletRequest request) {
+		BaseVO baseVO = new BaseVO();
+		String phone = request.getParameter("phone");
+		if(phone==null || phone.length() != 11){
+			baseVO.setBaseVO(BaseVO.FAILURE, "请输入正确的手机号");
+		}else{
+			//此参数决定最终是否可发送短信验证码
+			boolean send = false;
+			
+			//查询当前手机号是否已发送满条件
+			if(SmsLog.everyDayPhoneNum > 0){
+				int phoneNum = findByPhoneNum(phone, SmsLog.TYPE_LOGIN);
+				if(phoneNum<SmsLog.everyDayPhoneNum){
+					send = true;
+				}else{
+					baseVO.setBaseVO(BaseVO.FAILURE, "此手机当天验证码发送已达上限！请明日再进行尝试");
+					send = false;
+				}
+			}else{
+				send = true;
+			}
+			
+			if(send){
+				if(SmsLog.everyDayIpNum > 0){
+					int ipNum = findByIpNum(IpUtil.getIpAddress(request), SmsLog.TYPE_LOGIN);
+					if(ipNum<SmsLog.everyDayIpNum){
+						send = true;
+					}else{
+						baseVO.setBaseVO(BaseVO.FAILURE, "此IP当天验证码发送已达上限！请明日再进行尝试");
+						send = false;
+					}
+				}
+			}
+			
+			//发送验证码流程逻辑
+			if(send){
+				Random random = new Random();
+				String code = random.nextInt(10)+""+random.nextInt(10)+""+random.nextInt(10)+""+random.nextInt(10)+""+random.nextInt(10)+""+random.nextInt(10);
+				
+				SmsLog smsLog = new SmsLog();
+				smsLog.setAddtime(DateUtil.timeForUnix10());
+				smsLog.setCode(code);
+				smsLog.setIp(IpUtil.getIpAddress(request));
+				smsLog.setPhone(phone);
+				smsLog.setType(SmsLog.TYPE_LOGIN);
+				smsLog.setUsed(SmsLog.USED_FALSE);
+				smsLog.setUserid(0);
+				save(smsLog);
+				
+				if(smsLog.getId()>0){
+					SendPhoneMsgUtil.send(phone, code);
+					baseVO.setBaseVO(BaseVO.SUCCESS, "验证码已发送至您手机");
+				}else{
+					baseVO.setBaseVO(BaseVO.FAILURE, "验证码保存失败！");
+				}
+			}
+		}
+		
+		return baseVO;
 	}
 
 }
