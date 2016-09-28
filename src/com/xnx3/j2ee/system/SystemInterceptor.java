c9939ea03db0ed70aeeae292b4a13160310bde85
xnx3 <mail@xnx3.com>
2016-09-28 19:46:49 +0800

    xnx3
    增加 函数运行时间监控  Controller 、Implement 、 Dao

--- src/com/xnx3/j2ee/system/SystemInterceptor.java
+++ src/com/xnx3/j2ee/system/SystemInterceptor.java
@@ -1,4 +1,4 @@
-package com.xnx3.j2ee.interceptor;
+package com.xnx3.j2ee.system;
 
 import java.util.List;
 
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.xnx3.ConfigManagerUtil;
+import com.xnx3.DateUtil;
+import com.xnx3.Lang;
 import com.xnx3.j2ee.Global;
 import com.xnx3.j2ee.service.MessageService;
 import com.xnx3.j2ee.entity.Message;
@@ -26,11 +28,18 @@ import com.xnx3.j2ee.util.CookieUtil;
  */
 public class SystemInterceptor extends HandlerInterceptorAdapter {
 	public static boolean useMessage=false;	//是否使用站内信息功能	
+	
+	public static boolean useExecuteTime = false;	//是否使用Controller函数记录执行时间的功能
+	public static long recordTime = 0;	//若执行时间超过多少毫秒，就在控制台打印出来，这里的单位是毫秒
+	
 	@Resource
 	private MessageService messageService;
 	
 	static{
 		useMessage = ConfigManagerUtil.getSingleton("systemConfig.xml").getValue("message.used").equals("true");
+		useExecuteTime = ConfigManagerUtil.getSingleton("systemConfig.xml").getValue("ExecuteTime.controller.used").equals("true");
+		int recordTimeInt = Lang.stringToInt(ConfigManagerUtil.getSingleton("systemConfig.xml").getValue("ExecuteTime.recordTime"), 0);
+		recordTime = recordTimeInt;
 	}
 	
 	@Override
@@ -45,22 +54,24 @@ public class SystemInterceptor extends HandlerInterceptorAdapter {
 			ModelAndView modelAndView) throws Exception {
 		//从shiro的session中取activeUser
 		Subject subject = SecurityUtils.getSubject();
-		//取身份信息
-		ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
-		if(activeUser!=null){
-			User user=activeUser.getUser();
-			
-			//站内信
-			if(useMessage){
-				Message messageWhere=new Message();
-				messageWhere.setRecipientid(user.getId());
-				messageWhere.setState(Message.MESSAGE_STATE_UNREAD);
-				List<Message> list = messageService.findByExample(messageWhere);
+		if(subject != null){
+			//取身份信息
+			ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+			if(activeUser!=null){
+				User user=activeUser.getUser();
 				
-				if(modelAndView!=null){
-					modelAndView.addObject("user", user);
-					modelAndView.addObject("messageList", list);
-					modelAndView.addObject("unreadMessage", list.size());
+				//站内信
+				if(useMessage){
+					Message messageWhere=new Message();
+					messageWhere.setRecipientid(user.getId());
+					messageWhere.setState(Message.MESSAGE_STATE_UNREAD);
+					List<Message> list = messageService.findByExample(messageWhere);
+					
+					if(modelAndView!=null){
+						modelAndView.addObject("user", user);
+						modelAndView.addObject("messageList", list);
+						modelAndView.addObject("unreadMessage", list.size());
+					}
 				}
 			}
 		}
@@ -83,12 +94,26 @@ public class SystemInterceptor extends HandlerInterceptorAdapter {
 			request.getSession().setAttribute("language_default", Global.language_default);
 		}
 		
+		if(useExecuteTime){
+			long startTime = (Long)request.getAttribute("startTime");  
+	        long endTime = System.currentTimeMillis();  
+	        long executeTime = endTime - startTime;  
+	        
+	        if(executeTime > recordTime){
+	        	System.out.println(DateUtil.currentDate("MM-dd HH:mm:ss")+" ControllerExecuteTime : "+executeTime+" ms , "+handler);
+			}
+		}
+        
 		super.postHandle(request, response, handler, modelAndView);
 	}
 
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
+		if(useExecuteTime){
+			long startTime = System.currentTimeMillis();  
+	        request.setAttribute("startTime", startTime);  
+		}
 		return true;
 	}
 }
