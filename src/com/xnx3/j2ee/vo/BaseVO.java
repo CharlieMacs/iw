592cf20cd94120ff29ce8339f6a86b5fbd57bfc8
xnx3 <mail@xnx3.com>
2017-02-15 08:40:16 +0800

    xnx3
    com.xnx3.j2ee.vo.BaseVO 修改，继承 com.xnx3.BaseVO

--- src/com/xnx3/j2ee/vo/BaseVO.java
+++ src/com/xnx3/j2ee/vo/BaseVO.java
@@ -7,74 +7,18 @@ package com.xnx3.j2ee.vo;
  * @author 管雷鸣
  *
  */
-public class BaseVO {
-	public final static int SUCCESS=1;
-	public final static int FAILURE=0;
+public class BaseVO extends com.xnx3.BaseVO {
 	
-	/*
-	 * 执行结果
-	 * <li>0:失败
-	 * <li>1:成功
-	 */
-	private int result;
-	
-	/*
-	 * 执行信息,如执行成功，会返回执行成功的信息，执行失败，会返回为什么会失败的信息
-	 */
-	private String info;
-	
-	/**
-	 * 默认为成功状态
-	 */
-	public BaseVO() {
-		this.result = SUCCESS;
-		this.info = "成功";
+	public BaseVO(){
+		super();
 	}
 	
 	/**
-	 * @param result 执行状态
-	 * 				<li>{@link BaseVO#SUCCESS} 	：失败
-	 * 				<li>{@link BaseVO#FAILURE} 	：成功
-	 * @param info 执行信息,如执行成功，会返回执行成功的信息，执行失败，会返回为什么会失败的信息
+	 * 将 {@link com.xnx3.BaseVO} 转为 {@link com.xnx3.j2ee.vo.BaseVO}
+	 * @param baseVO
 	 */
-	public void setBaseVO(int result,String info) {
-		this.result = result;
-		this.info = info;
-	}
-	
-	/**
-	 * 将另一个 BaseVO 的信息赋值到这个上。克隆
-	 * @param baseVO {@link BaseVO}
-	 */
-	public void setBaseVO(BaseVO baseVO) {
-		this.result = baseVO.getResult();
-		this.info = baseVO.getInfo();
-	}
-	
-	public int getResult() {
-		return result;
-	}
-
-	/**
-	 * 执行结果
-	 * @param result
-	 * 		<li>{@link BaseVO#SUCCESS} 	：失败
-	 * 		<li>{@link BaseVO#FAILURE} 	：成功
-	 */
-	public void setResult(int result) {
-		this.result = result;
-	}
-
-	public String getInfo() {
-		return info;
-	}
-
-	/**
-	 * 执行信息,如执行成功，会返回执行成功的信息，执行失败，会返回为什么会失败的信息
-	 * @param info
-	 */
-	public void setInfo(String info) {
-		this.info = info;
+	public void setBaseVOForSuper(com.xnx3.BaseVO baseVO){
+		setBaseVO(baseVO.getResult(), baseVO.getInfo());
 	}
 	
 }
